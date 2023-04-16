87ac525201c2fd50f32f87c29b3737b5403f95bb
Darika Kazybaeva <kazybaeva_d@auca.kg>
2023-04-16 17:44:36 +0600

    program finished

---
+++ TechnicalTask/src/CronParser.java
@@ -0,0 +1,132 @@
+public class CronParser {
+    final static int minuteMin = 0, minuteMax = 59;
+    final static int hourMin = 0, hourMax = 23;
+    final static int dayMin = 1, dayMax = 31;
+    final static int monthMin = 1, monthMax = 12;
+    final static int dayOfWeekMin = 0, dayOfWeekMax = 6;
+
+    StringBuilder minute, hour, day, month, dayOfWeek, command;
+    String[] arr;
+    final int numOfFields = 6;
+
+    CronParser(String cronExp) throws Exception {
+        arr = cronExp.trim().split("\\s+");
+
+        if (arr.length != numOfFields) {
+            throw new Exception(
+                    "Invalid input format: " +
+                    "The standard cron format consists of 6 fields " +
+                    "minute, hour, day of month, month, and day of week, " +
+                    "command");
+        }
+    }
+
+    public void expressionParsing() throws Exception {
+        String minuteTemp = unitParsing("minute", arr[0], minuteMin, minuteMax);
+        String hourTemp = unitParsing("hour", arr[1], hourMin, hourMax);
+        String dayTemp = unitParsing("day", arr[2], dayMin, dayMax);
+        String monthTemp = unitParsing("month", arr[3], monthMin, monthMax);
+        String dayOfWeekTemp = unitParsing("dayOfWeek", arr[4], dayOfWeekMin, dayOfWeekMax);
+        String commandTemp = arr[5];
+
+        minute = new StringBuilder(String.format("%-14s", "minute")).append(minuteTemp);
+        hour = new StringBuilder(String.format("%-14s", "hour")).append(hourTemp);
+        day = new StringBuilder(String.format("%-14s", "day of month")).append(dayTemp);
+        month = new StringBuilder(String.format("%-14s", "month")).append(monthTemp);
+        dayOfWeek = new StringBuilder(String.format("%-14s", "day of week")).append(dayOfWeekTemp);
+        command = new StringBuilder(String.format("%-14s", "command")).append(commandTemp);
+    }
+
+    public String unitParsing(String field, String s, int min, int max) throws Exception {
+        String parsedUnit;
+
+        if (s.contains("/")) {
+            parsedUnit = forwardSlash(field, s, min, max);
+        } else if (s.contains(",")) {
+            parsedUnit = comma(field, s, min, max);
+        } else if (s.contains("-")) {
+            parsedUnit = hyphen(field, s, min, max);
+        } else if (s.contains("*")) {
+            parsedUnit = asterisk(min, max);
+        } else {
+            isWithinRange(field, Integer.parseInt(s), min, max);
+            parsedUnit = s;
+        }
+
+        return parsedUnit;
+    }
+
+    public void isWithinRange(String field, int value, int min, int max) throws Exception {
+        if (value < min || value > max) {
+            throw new Exception("Incorrect value: " + value + ". The " + field + " value must be within the range [" + min + ", " + max + "]");
+        }
+    }
+
+    public String asterisk(int min, int max) {
+        StringBuilder res = new StringBuilder();
+
+        for (int i = min; i <= max; i++) {
+            res.append(i).append(" ");
+        }
+
+        return res.toString();
+    }
+
+    public String comma(String field, String s, int min, int max) throws Exception {
+        String[] values = s.split(",");
+        StringBuilder res = new StringBuilder();
+
+        for (String value : values) {
+            isWithinRange(field, Integer.parseInt(value), min, max);
+            res.append(value).append(" ");
+        }
+
+        return res.toString();
+    }
+
+    public String hyphen(String field, String s, int min, int max) throws Exception {
+        int minOfRange = Integer.parseInt(s.substring(0, s.indexOf("-")));
+        isWithinRange(field, minOfRange, min, max);
+
+        int maxOfRange = Integer.parseInt(s.substring(s.indexOf("-") + 1));
+        isWithinRange(field, maxOfRange, min, max);
+
+        StringBuilder res = new StringBuilder();
+
+        for (int i = minOfRange; i <= maxOfRange; i++) {
+            res.append(i).append(" ");
+        }
+
+        return res.toString();
+    }
+
+    public String forwardSlash(String field, String s, int min, int max) throws Exception {
+        StringBuilder res = new StringBuilder();
+        String startValue = s.substring(0, s.indexOf("/")); //startValue = 15-40
+        int start;
+        int end = max;
+
+        if (startValue.length() > 2) { //true
+            String range = hyphen(field, startValue, min, max); //range "15 16 ... 39 40 "
+            String[] rangeValues = range.split("\\s+"); // rangeValues = [15, 16, ..., 39, 40]
+            start = Integer.parseInt(rangeValues[0]); //15
+            end = Integer.parseInt(rangeValues[rangeValues.length - 1]); //40
+        } else {
+            if (startValue.equals("*")) {
+                start = 0;
+            } else {
+                start = Integer.parseInt(startValue); //example 15/5
+                isWithinRange(field, start, min, max);
+            }
+        }
+
+        int step = Integer.parseInt(s.substring(s.indexOf("/") + 1));
+        isWithinRange(field, step, min, max);
+
+        for (int i = start; i <= end; i += step) {
+            res.append(i).append(" ");
+        }
+
+        return res.toString();
+    }
+}
--- TechnicalTask/src/Main.java
+++ TechnicalTask/src/Main.java
@@ -1,77 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    //input sample
-    //*/15 0 1,15 * 1-5 /usr/bin/find
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String cronExp = input.nextLine();
-        String[] arr = cronExp.trim().split("\\s+");
 
-        System.out.println(asterisk(arr[3], 1, 12));
-        System.out.println(comma(arr[2]));
-        System.out.println(hyphen(arr[4]));
-        System.out.println(forwardSlash(arr[0], 59));
-    }
-
-    public static String asterisk(String s, int min, int max) {
-        //apriori check for the presence of asterisk
-        StringBuilder res = new StringBuilder();
-
-        for (int i = min; i <= max; i++) {
-            res.append(i).append(" ");
+        try {
+            CronParser cp = new CronParser(cronExp);
+            cp.expressionParsing();
+            System.out.println(cp.minute);
+            System.out.println(cp.hour);
+            System.out.println(cp.day);
+            System.out.println(cp.month);
+            System.out.println(cp.dayOfWeek);
+            System.out.println(cp.command);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-
-        return res.toString();
-    }
-
-    public static String comma(String s) {
-        String[] values = s.split(",");
-        StringBuilder res = new StringBuilder();
-
-        for (String value : values) {
-            res.append(value).append(" ");
-        }
-
-        return res.toString();
-    }
-
-    public static String hyphen(String s) {
-        int minOfRange = Integer.parseInt(s.substring(0, s.indexOf("-")));
-        int maxOfRange = Integer.parseInt(s.substring(s.indexOf("-") + 1));
-        StringBuilder res = new StringBuilder();
-
-        for (int i = minOfRange; i <= maxOfRange; i++) {
-            res.append(i).append(" ");
-        }
-
-        return res.toString();
-    }
-
-    public static String forwardSlash(String s, int max) {
-        StringBuilder res = new StringBuilder();
-        int min;
-        String startValue = s.substring(0, s.indexOf("/")); //startValue = 15-40
-
-        if (startValue.length() > 2) { //true
-            String range = hyphen(startValue); //range "15 16 ... 39 40 "
-            String[] rangeValues = range.split("\\s+"); // rangeValues = [15, 16, ..., 39, 40]
-            min = Integer.parseInt(rangeValues[0]); //15
-            max = Integer.parseInt(rangeValues[rangeValues.length - 1]); //40
-        } else {
-            if (startValue.equals("*")) {
-                min = 0;
-            } else {
-                min = Integer.parseInt(s.substring(0, s.indexOf("/")));
-            }
-        }
-
-        int step = Integer.parseInt(s.substring(s.indexOf("/") + 1));
-
-        for (int i = min; i <= max; i += step) {
-            res.append(i).append(" ");
-        }
-
-        return res.toString();
     }
 }
